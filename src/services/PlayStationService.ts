de27b5098effb484e49c6e7020d6cdb2f34583ce
Олег Шуляков <olegshulyakov@MacBook-Pro-Oleg.local>
2020-02-25 23:55:48 +0300

    add additional methods in ps service

--- src/services/PlayStationService.ts
+++ src/services/PlayStationService.ts
@@ -4,7 +4,7 @@ const SORT_FIELD = Object.freeze({ RELEASE_DATE: "release_date", TIMESTAMP: "tim
 const SORT_DIRECTION = Object.freeze({ ASC: "asc", DESC: "desc" });
 
 export default class PlayStationService {
-    private readonly baseUrl = "https://store.playstation.com/store/api/chihiro/00_09_000/container";
+    private readonly baseUrl = "https://store.playstation.com/store/api/chihiro/00_09_000";
     private readonly rootCusa = "STORE-MSF75508-FULLGAMES";
     private language: string;
     private country: string;
@@ -15,10 +15,16 @@ export default class PlayStationService {
         this.country = country;
     }
 
+    async getGeoInfo() {
+        console.debug(`Quering geo info`);
+        const response = await fetch(`${this.baseUrl}//geo`);
+        return response;
+    }
+
     async query(cusa: string, size: number = 1, start: number = 0) {
         console.debug(`Quering cusa=${cusa}, start=${start}, size=${size}`);
         const response = await fetch(
-            `${this.baseUrl}/${this.language}/${this.country}/999/${cusa}?size=${size}&start=${start}&sort_default=${SORT_FIELD.TIMESTAMP}&sort_default_direction=${SORT_DIRECTION.DESC}`,
+            `${this.baseUrl}/container/${this.language}/${this.country}/999/${cusa}?size=${size}&start=${start}&sort_default=${SORT_FIELD.TIMESTAMP}&sort_default_direction=${SORT_DIRECTION.DESC}`,
         );
         return response;
     }
@@ -45,6 +51,10 @@ export default class PlayStationService {
         const json = await response.json();
         //console.debug(`Game list response: ${JSON.stringify(json)}`);
         return json.links;
+    }
+
+    getGameImageLink(cusa: string, width: number = 240, height: number = 240): string {
+        return `${this.baseUrl}/titlecontainer/${this.country}/${this.language}/999/${cusa}/image?w=${width}&h=${height}`;
     }
 
     getStoreGameLink(cusa: string): string {
