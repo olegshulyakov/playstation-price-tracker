db0a7b35b98e890878158595cd03ebe422378309
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2020-03-16 00:12:25 +0300

    preview generator

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,11 +14,9 @@
  * limitations under the License.
  */
 
-// import * as functions from "firebase-functions";
+import previewGenerator from "./previewGenerator";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+export { previewGenerator };
