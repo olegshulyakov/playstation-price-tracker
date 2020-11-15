ad410072db3859abbfcbdb159d9d34382bf7ca88
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2020-11-16 00:44:00 +0300

    Updates

---
+++ precache-manifest.ffed6daa501c2df1818398cf2d8c2f87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399ed087329bc538c6a7f1814558bc0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb92f786a77f8a0b9284",
+    "url": "/static/css/2.090590db.chunk.css"
+  },
+  {
+    "revision": "4e8e5c2409df90cd61aa",
+    "url": "/static/css/main.2e9e6f81.chunk.css"
+  },
+  {
+    "revision": "bb92f786a77f8a0b9284",
+    "url": "/static/js/2.a0db8e04.chunk.js"
+  },
+  {
+    "revision": "434eeea35bf11e3d22931f972c8fbb4c",
+    "url": "/static/js/2.a0db8e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8e5c2409df90cd61aa",
+    "url": "/static/js/main.f4f625c4.chunk.js"
+  },
+  {
+    "revision": "f1adb009e1a56ffd569a",
+    "url": "/static/js/runtime-main.a3775064.js"
+  },
+  {
+    "revision": "402fbb90320a3eb53ecbf24e69d0fcc9",
+    "url": "/static/media/playstation-classic.402fbb90.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game-price-tracker/precache-manifest.b07170d4fb5714d98bf855269b0a7eb0.js"
+  "/precache-manifest.ffed6daa501c2df1818398cf2d8c2f87.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game-price-tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
