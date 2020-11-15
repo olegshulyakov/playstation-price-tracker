64eb4b42188e805b52dd709dfce910054daa527d
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2020-11-16 00:56:47 +0300

    Updates

---
+++ precache-manifest.fb1a7b7ff7d52245244966e6fb2e7c07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8a58f97a38987ee6b9275ed21c1894",
+    "url": "/game-price-tracker/index.html"
+  },
+  {
+    "revision": "d6b1d492575d7b70b346",
+    "url": "/game-price-tracker/static/css/2.090590db.chunk.css"
+  },
+  {
+    "revision": "149398137de24dc112bd",
+    "url": "/game-price-tracker/static/css/main.2e9e6f81.chunk.css"
+  },
+  {
+    "revision": "d6b1d492575d7b70b346",
+    "url": "/game-price-tracker/static/js/2.fc63b7d4.chunk.js"
+  },
+  {
+    "revision": "434eeea35bf11e3d22931f972c8fbb4c",
+    "url": "/game-price-tracker/static/js/2.fc63b7d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "149398137de24dc112bd",
+    "url": "/game-price-tracker/static/js/main.2af168cb.chunk.js"
+  },
+  {
+    "revision": "d68fc54b6920870dcee4",
+    "url": "/game-price-tracker/static/js/runtime-main.d0811e63.js"
+  },
+  {
+    "revision": "402fbb90320a3eb53ecbf24e69d0fcc9",
+    "url": "/game-price-tracker/static/media/playstation-classic.402fbb90.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ffed6daa501c2df1818398cf2d8c2f87.js"
+  "/game-price-tracker/precache-manifest.fb1a7b7ff7d52245244966e6fb2e7c07.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game-price-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
