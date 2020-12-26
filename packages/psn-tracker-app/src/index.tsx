5948f0a3f5568f093c78ed2c18b6994817f8106a
Oleg Shulyakov <olegshulyakov@users.noreply.github.com>
2020-12-26 15:50:55 +0300

    fix github pages

--- packages/psn-tracker-app/src/index.tsx
+++ packages/psn-tracker-app/src/index.tsx
@@ -18,7 +18,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 import App from "./App";
@@ -43,7 +43,7 @@ localStorage.setItem(APP_VERSION, appVersion);
 ReactDOM.render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-            <BrowserRouter>
+            <Router>
                 <Switch>
                     <Route path="/" exact component={() => <App />} />
                     <Route path="/selectregion" exact component={() => <SelectRegion />} />
@@ -54,7 +54,7 @@ ReactDOM.render(
                     <Route path="/about" exact component={() => <About />} />
                     <Route path="/game/:cusa" component={() => <GameDetail />} />
                 </Switch>
-            </BrowserRouter>
+            </Router>
         </PersistGate>
     </Provider>,
     document.getElementById("root"),
